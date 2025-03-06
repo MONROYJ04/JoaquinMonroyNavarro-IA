c5dbf31b2a63775fb5f84ec44dfdb561a95a1544
JOAQUIN MONROY NAVARRO <A22310267@live.ceti.mx>
2025-03-05 19:39:14 -0600

    0002_The print function_22310267_JoaquinMonroy
    Se realizo el uso de la funcion print empleando  tipos de argumentos

---
+++ 0002_The print function_22310267_JoaquinMonroy.py
@@ -0,0 +1,23 @@
+#El \n es un salto de linea
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+print()
+
+#La coma es para que haya un espacio entre las palabras
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+print()
+
+#El end=" " es para que no haya salto de linea
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+print()
+
+#El sep="-" es para que haya un guion entre las palabras
+print("My", "name", "is", "Monty", "Python.", sep="-")
+print()
+print()
+
+#Ejemplo de todo junto
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
