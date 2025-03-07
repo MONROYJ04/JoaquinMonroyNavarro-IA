8a32da62459ebaa4e307cf36d24263133fdca1f8
JOAQUIN MONROY NAVARRO <A22310267@live.ceti.mx>
2025-03-06 20:51:58 -0600

    0003_VariablesVersion1_22310267_JoaquinMonroy
    Se hizo el uso de variables y operadores de forma basica

---
+++ 0003_VariablesVersion1_22310267_JoaquinMonroy.py
@@ -0,0 +1,8 @@
+var=1
+print(var)
+balance_cuenta=1000.0
+nombre_cliente="Joaquin Monroy"
+print(var,nombre_cliente,balance_cuenta,sep="_")
+balance_cuenta=1000.0+500.50
+print()
+print(balance_cuenta)
