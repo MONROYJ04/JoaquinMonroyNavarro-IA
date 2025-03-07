bf0515545ffdc702bb9fd99447c8188de70a41f0
JOAQUIN MONROY NAVARRO <A22310267@live.ceti.mx>
2025-03-06 20:52:54 -0600

    0004_VariablesVersion2_22310267_JoaquinMonroy
    Se utilizo variables y operadores para resolver operaciones

---
+++ 0004_VariablesVersion2_22310267_JoaquinMonroy.py
@@ -0,0 +1,25 @@
+a=10
+b=5
+c=(a**2+b**2)**0.5
+print("c=",c,"\n")
+
+
+Josue=10
+Braulio=5
+Carlos=6
+print("Josue=",Josue,"Braulio=",Braulio,"Carlos=",Carlos,sep=",")
+total_manzanas=Josue*Braulio*Carlos
+print("total_manzanas=",total_manzanas,"\n")
+Carlos +=5
+print("Carlos=",Carlos)
+
+
+
+pies=1.0
+pulgadas=12.0
+
+pies_a_pulgadas=pies*pulgadas
+pulgasdas_a_pies=pies/pulgadas
+
+print(pies,"pies es",round(pies_a_pulgadas), "pulgadas")
+print(pulgadas,"pulgadas es",round(pulgasdas_a_pies), "pies")
