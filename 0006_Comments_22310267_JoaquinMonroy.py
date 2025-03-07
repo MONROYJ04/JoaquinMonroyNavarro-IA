81967aefab901330c5e1a32baf9169929302175b
JOAQUIN MONROY NAVARRO <A22310267@live.ceti.mx>
2025-03-06 21:07:24 -0600

    0006_Comments_22310267_JoaquinMonroy
    En este commit se hace uso de los comentarios en diversas formas

---
+++ 0006_Comments_22310267_JoaquinMonroy.py
@@ -0,0 +1,15 @@
+#Esta practuca trata sobre usar comentarios 
+
+#Creamos la variable a
+a= '1'
+#Creamos la variable b
+b= "2"
+#Imprimimos la suma de a y b
+print(a+b) # 12
+
+#print(a)
+
+#Para poder hacer un buen uso de los comentarios es importante que estos sean claros y concisos, de esta manera se entendera mejor el codigo
+#Ademas de que se puede usar para desactivar una linea de codigo que no queremos que se ejecute en ese momento
+#Tambien se pueden usar para hacer anotaciones sobre el codigo que estamos escribiendo
+#O simplemente para hacer que el codigo sea mas legible
