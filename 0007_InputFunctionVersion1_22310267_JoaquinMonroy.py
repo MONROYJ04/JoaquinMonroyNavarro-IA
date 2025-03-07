ffbda1fb6a1be02687cb38d42a90320b9fd5e67a
JOAQUIN MONROY NAVARRO <A22310267@live.ceti.mx>
2025-03-06 21:25:01 -0600

    0007_InputFunction_22310267_JoaquinMonroy
    En este commit se emplea la funcion input para solicitar informacion al usuario

---
+++ 0007_InputFunctionVersion1_22310267_JoaquinMonroy.py
@@ -0,0 +1,18 @@
+print("Ingrese su nombre:\n")
+nombre = input()
+print("Ingrese su edad:\n")
+edad = input()
+print("Hola, " + nombre + " tienes " + edad + " años")
+print("Gracias por usar el programa\n")
+
+
+print("Hola, bienvenido")
+print("Por favor, ingrese un numero con decimales")
+numero1=float(input())
+print("El numero que ingresaste es: ", numero1, "\n")
+
+print("Ingrese su alrtura en metros")
+altura1=float(input())
+print("Ingrese la altura de su hermano")
+altura2=float(input())
+print("La diferencia de altura entre ustedes es: ", altura1-altura2, "metros\n")
