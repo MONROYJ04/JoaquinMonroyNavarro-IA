ebe22163587cae4580d3d415f8ab95a1019a65c3
JOAQUIN MONROY NAVARRO <A22310267@live.ceti.mx>
2025-03-15 02:49:22 -0600

    0017_IndexacionListas_22310267_JoaquinMonroy
    Se aprendio a crear una lista y acceder a los elementos de esta

---
+++ 0017_IndexacionListas_22310267_JoaquinMonroy.py
@@ -0,0 +1,13 @@
+#Permite acceder a los elementos de una lista mediante su índice
+
+#Crear una lista
+frutas = ["manzana", "pera", "uva", "sandía", "mango"]
+
+#Acceder a los elementos de la lista mediante su índice
+print(frutas[0]) #manzana
+print(frutas[3]) #sandía
+print(frutas[1]) #pera
+print(frutas[4]) #mango
+print(frutas[2]) #uva
+
+
