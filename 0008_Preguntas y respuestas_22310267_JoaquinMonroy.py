1b71e5a4c3440936ff21111ecc153031de4fa96b
JOAQUIN MONROY NAVARRO <A22310267@live.ceti.mx>
2025-03-14 22:42:57 -0600

    0008_Preguntas y Respuestas_22310267_JoaquinMonroy
    Se realizo el uso del operador de igualdad para formular preguntas y respuestas.

---
+++ 0008_Preguntas y respuestas_22310267_JoaquinMonroy.py
@@ -0,0 +1,10 @@
+#En este codigo se usara el operador de igualdad para preguntas y respuestas 
+
+print(5 == 5)
+print(10==20)
+print(15.20==15.2)
+print("hola"=="hola")
+print("hola"=="Hola")
+print({"a":22, "b":33} == {"b":33, "a":22})
+print()
+
