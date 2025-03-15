b3c39ce7eef2dea56d77fe05046d3e1489a163c3
JOAQUIN MONROY NAVARRO <A22310267@live.ceti.mx>
2025-03-14 22:54:41 -0600

    0009_Operadores_22310267_JoaquinMonroy
    Se empleo los diversos operadores para formular y responder preguntas

---
+++ 0009_Operadores_22310267_JoaquinMonroy.py
@@ -0,0 +1,27 @@
+#En esta practica usaremos los operadores para diversos casos 
+
+#Igualdad
+print(5 == 5,"\n") 
+
+#Desigualdad
+print(5 != 5,"\n")
+
+#Mayor que
+print(5 > 25)
+print(25 > 5,"\n")
+
+#Menor que
+print(15.6666 < 15.6667)
+print(15.6667 < 15.6666,"\n")
+
+#Ejericio 
+edad_pablo=input("Introduce la edad de Pablo: ")#Se pide la edad de Pablo
+edad_pedro=input("Introduce la edad de Pedro: ")#Se pide la edad de Pedro
+#Se comparan las edades y se imprime el resultado
+if edad_pablo > edad_pedro:
+    print("Pablo es mayor que Pedro")
+elif edad_pablo < edad_pedro:
+        print("Pedro es mayor que Pablo")
+else:
+    print("Pablo y Pedro tienen la misma edad")
+    
